6898abb578067cb9a9254c737f3ac8c73fd15747
kylemann16 <kyle@hobu.co>
2026-07-31 10:34:33 -0500

    adding REQUESTS_CA_BUNDLE + AWS_CA_BUNDLE to lambda, setting ca_cert_file in duck usage

--- terraform/resources/lambdas/compare.tf
+++ terraform/resources/lambdas/compare.tf
@@ -1,3 +1,7 @@
+locals {
+    lambda_cert_dest = "/tmp/cert.pem"
+}
+
 resource aws_lambda_function compare_function {
 
     function_name = "${var.prefix}_tns_comp_lambda"
@@ -21,6 +25,8 @@ resource aws_lambda_function compare_function {
             DEPLOY_PREFIX: var.prefix
             MEMORY_LIMIT: var.memory_size
             S3_CERT_PATH: var.s3_cert_path
+            REQUESTS_CA_BUNDLE: local.lambda_cert_dest
+            AWS_CA_BUNDLE: local.lambda_cert_dest
             AWS_S3_ENDPOINT: var.s3_endpoint
         }
     }
